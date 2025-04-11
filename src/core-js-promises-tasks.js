e7433df76f2cecc519f835276d61874a6a1b42a1
turik777 <52492191+turik777@users.noreply.github.com>
2025-04-12 01:23:04 +0300

    feat: complete queuePromises function

--- src/core-js-promises-tasks.js
+++ src/core-js-promises-tasks.js
@@ -161,8 +161,14 @@ function getAllResult(promises) {
  * [promise1, promise4, promise3] => Promise.resolved('104030')
  * [promise1, promise4, promise3, promise2] => Promise.resolved('10403020')
  */
-function queuePromises(/* promises */) {
-  throw new Error('Not implemented');
+function queuePromises(promises) {
+  return promises.reduce(
+    (chain, promise) =>
+      chain.then((string) => promise.then((value) => string + value)),
+    new Promise((resolve) => {
+      resolve('');
+    })
+  );
 }
 
 module.exports = {
